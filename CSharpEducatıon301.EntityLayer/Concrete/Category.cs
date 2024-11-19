521c9c96e633e774c31044f1318346a03cd3472b
Mehmetmustafa46 <mehmetmustafaakgul47@gmail.com>
2024-11-19 21:16:24 +0300

    Add project files.

---
+++ CSharpEducatıon301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEducatıon301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+       public List<Product> Products { get; set; }
+
+    }
+}
+/*
+ Field-Variable-Property
+ */
+/*
+ int x;--> Field (Get; Set; Property olur)
+ */
